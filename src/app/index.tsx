3e62575afe66957b591c166999482b63eed8a4c5
LuizF3lipe <luizf3lipe.dev@gmail.com>
2024-04-03 17:25:46 -0300

    aula 1

---
+++ src/app/index.tsx
@@ -0,0 +1,48 @@
+import { View, Image, StatusBar } from "react-native"
+import { Link } from "expo-router"
+import { MaterialCommunityIcons } from "@expo/vector-icons"
+
+import { Input } from "@/components/Input"
+import { Button } from "@/components/Button"
+
+import { colors } from "@/styles/colors"
+
+export default function Home() {
+  return (
+    <View className="flex-1 bg-green-500 items-center justify-center p-8">
+      <StatusBar barStyle="light-content" />
+
+      <Image 
+        className="h-16"
+        resizeMode="contain"
+        source={require("@/assets/logo.png")} 
+      />
+      <View
+        className="w-full mt-12 gap-3"
+      >
+        <Input>
+          <MaterialCommunityIcons 
+            name="ticket-confirmation-outline"
+            size={20}  
+            color={colors.green[200]}
+          />
+
+          <Input.Field 
+            placeholder="Código do ingresso"
+          />
+        </Input>
+
+        <Button 
+          title="Acessar Credencial"
+        />
+
+        <Link 
+          href="/register"
+          className="text-gray-100 text-base font-bold text-center mt-8"
+          >
+            Ainda não possui ingresso?
+          </Link>
+      </View>
+    </View>
+  )
+}
